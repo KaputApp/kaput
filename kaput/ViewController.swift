cae7525f8214ad88763870d06e6eb6239ac211b3
Noémie Rebibo <momy_guibo@hotmail.com>
2016-05-26 18:59:01 +0200

    Initial Commit

---
+++ kaput/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kaput
+//
+//  Created by Noémie Rebibo on 26/05/2016.
+//  Copyright © 2016 OPE50. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
