ef6dcf567593d8388d06947ee52c484468c6f97f
KaputApp <illan.simon@hotmail.fr>
2016-06-03 20:27:59 +0200

    transition segue

--- kaput/KaputStyleFactory.swift
+++ kaput/KaputStyleFactory.swift
@@ -14,6 +14,59 @@ var batteryLevel = batLevel.init().levelBat
 var stateBattery = batLevel.init().stateBat
 
 
+public class SegueFromLeft: UIStoryboardSegue
+{
+    override public func perform() {
+        let toViewController = destinationViewController
+        let fromViewController = sourceViewController
+        
+        let containerView = fromViewController.view.superview
+        let screenBounds = UIScreen.mainScreen().bounds
+        
+        let finalToFrame = screenBounds
+        let finalFromFrame = CGRectOffset(finalToFrame, -screenBounds.size.width, 0)
+        
+        toViewController.view.frame = CGRectOffset(finalToFrame, screenBounds.size.width, 0)
+        containerView?.addSubview(toViewController.view)
+        
+        UIView.animateWithDuration(0.25, animations: {
+            toViewController.view.frame = finalToFrame
+            fromViewController.view.frame = finalFromFrame
+            }, completion: { finished in
+                let fromVC = self.sourceViewController
+                let toVC = self.destinationViewController
+                fromVC.presentViewController(toVC, animated: false, completion: nil)
+        })
+    }
+}
+
+public class SegueFromRight: UIStoryboardSegue
+{
+    override public func perform() {
+        let toViewController = destinationViewController
+        let fromViewController = sourceViewController
+        
+        let containerView = fromViewController.view.superview
+        let screenBounds = UIScreen.mainScreen().bounds
+        
+        let finalToFrame = screenBounds
+        let finalFromFrame = CGRectOffset(finalToFrame, screenBounds.size.width, 0)
+        
+        toViewController.view.frame = CGRectOffset(finalToFrame, -screenBounds.size.width, 0)
+        containerView?.addSubview(toViewController.view)
+        
+        UIView.animateWithDuration(0.25, animations: {
+            toViewController.view.frame = finalToFrame
+            fromViewController.view.frame = finalFromFrame
+            }, completion: { finished in
+                let fromVC = self.sourceViewController
+                let toVC = self.destinationViewController
+                fromVC.presentViewController(toVC, animated: false, completion: nil)
+        })
+    }
+}
+
+
 class batLevel{
     var levelBat:Int
     var stateBat:UIDeviceBatteryState
@@ -119,64 +172,4 @@ public class Colors {
     
         
     }
-    
-
-
-    
-    // Segue Transition Style
-    
-public class SegueFromLeft: UIStoryboardSegue
-    {
-        override public func perform() {
-            let toViewController = destinationViewController
-            let fromViewController = sourceViewController
-            
-            let containerView = fromViewController.view.superview
-            let screenBounds = UIScreen.mainScreen().bounds
-            
-            let finalToFrame = screenBounds
-            let finalFromFrame = CGRectOffset(finalToFrame, -screenBounds.size.width, 0)
-            
-            toViewController.view.frame = CGRectOffset(finalToFrame, screenBounds.size.width, 0)
-            containerView?.addSubview(toViewController.view)
-            
-            UIView.animateWithDuration(0.25, animations: {
-                toViewController.view.frame = finalToFrame
-                fromViewController.view.frame = finalFromFrame
-                }, completion: { finished in
-                    let fromVC = self.sourceViewController
-                    let toVC = self.destinationViewController
-                    fromVC.presentViewController(toVC, animated: false, completion: nil)
-            })
-        }
-    }
-    
-    public class SegueFromRight: UIStoryboardSegue
-    {
-        override public func perform() {
-            let toViewController = destinationViewController
-            let fromViewController = sourceViewController
-            
-            let containerView = fromViewController.view.superview
-            let screenBounds = UIScreen.mainScreen().bounds
-            
-            let finalToFrame = screenBounds
-            let finalFromFrame = CGRectOffset(finalToFrame, screenBounds.size.width, 0)
-            
-            toViewController.view.frame = CGRectOffset(finalToFrame, -screenBounds.size.width, 0)
-            containerView?.addSubview(toViewController.view)
-            
-            UIView.animateWithDuration(0.25, animations: {
-                toViewController.view.frame = finalToFrame
-                fromViewController.view.frame = finalFromFrame
-                }, completion: { finished in
-                    let fromVC = self.sourceViewController
-                    let toVC = self.destinationViewController
-                    fromVC.presentViewController(toVC, animated: false, completion: nil)
-            })
-        }
-    }
-
-    
-
 }
