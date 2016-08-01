bcb0eef92afa34dbdaef5d238ef6833d829c5227
KaputApp <illan.simon@hotmail.fr>
2016-08-01 23:42:11 +0200

    avatar

--- kaput/EditProfileViewController.swift
+++ kaput/EditProfileViewController.swift
@@ -7,9 +7,87 @@
 //
 
 import UIKit
+import Photos
+import UIKit
+import Photos
+import Firebase
+import FirebaseAuth
+import FirebaseStorage
 
-class EditProfileViewController: UIViewController {
 
+class EditProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+
+    @IBAction func pickAvatar(sender: UIButton) {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        if (UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)) {
+            picker.sourceType = UIImagePickerControllerSourceType.Camera
+        } else {
+            picker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        }
+        
+        presentViewController(picker, animated: true, completion:nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        picker.dismissViewControllerAnimated(true, completion:nil)
+        
+        var storageRef:FIRStorageReference!
+        // if it's a photo from the library, not an image from the camera
+        if #available(iOS 8.0, *), let referenceUrl = info[UIImagePickerControllerReferenceURL] {
+            let assets = PHAsset.fetchAssetsWithALAssetURLs([referenceUrl as! NSURL], options: nil)
+            let asset = assets.firstObject
+            asset?.requestContentEditingInputWithOptions(nil, completionHandler: { (contentEditingInput,info) in
+                let imageFile = contentEditingInput?.fullSizeImageURL
+                let filePath = FIRAuth.auth()!.currentUser!.uid +
+                    "/\(Int(NSDate.timeIntervalSinceReferenceDate() * 1000))/\(imageFile!.lastPathComponent!)"
+                // [START uploadimage]
+                storageRef.child(filePath)
+                    .putFile(imageFile!, metadata: nil) { (metadata, error) in
+                        if let error = error {
+                            print("Error uploading: \(error)")
+
+                            return
+                        }
+                        self.uploadSuccess(metadata!, storagePath: filePath)
+                }
+                // [END uploadimage]
+            })
+//        } else {
+//            let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+//            let imageData = UIImageJPEGRepresentation(image, 0.8)
+//            let imagePath = FIRAuth.auth()!.currentUser!.uid +
+//                "/\(Int(NSDate.timeIntervalSinceReferenceDate() * 1000)).jpg"
+//            let metadata = FIRStorageMetadata()
+//            metadata.contentType = "image/jpeg"
+//            storageRef.child(imagePath)
+//                .putData(imageData!, metadata: metadata) { (metadata, error) in
+//                    if let error = error {
+//                        print("Error uploading: \(error)")
+//                        return
+//                    }
+//                    self.uploadSuccess(metadata!, storagePath: imagePath)
+//            }}
+        }}
+    
+    
+    
+    func uploadSuccess(metadata: FIRStorageMetadata, storagePath: String) {
+        print("Upload Succeeded!")
+//        self.urlTextView.text = metadata.downloadURL()!.absoluteString
+        NSUserDefaults.standardUserDefaults().setObject(storagePath, forKey: "storagePath")
+        NSUserDefaults.standardUserDefaults().synchronize()
+//        self.downloadPicButton.enabled = true
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        picker.dismissViewControllerAnimated(true, completion:nil)
+    }
+        
+        
+    
     @IBOutlet var mailField: kaputField!
     override func viewDidLoad() {
         
